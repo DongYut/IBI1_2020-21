73e77756a2e3c054ab45f145908b211ad3533081
yutiand <yutiand.20@intl.zju.edu.cn>
2021-03-10 13:14:48 +0800

    variable.py finished

---
+++ Practical5/variables.py
@@ -0,0 +1,22 @@
+a=180602
+b=190784
+c=100321
+d=a-c
+e=a-b
+if d>e:
+   print("d is greater")
+else: 
+   print("e is greater")
+
+
+x= 1<2
+y= 3>4
+z= (x and not y) or (y and not x)
+print(z)
+y= 3<4
+z= (x and not y) or (y and not x)
+print(z)
+
+w= x!=y
+print("w=",w)
+print("z=",z)
