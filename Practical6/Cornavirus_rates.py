2ed302522320c3b9b38580854feceff9859b9ec0
DongYut <79961144+DongYut@users.noreply.github.com>
2021-03-16 07:27:48 -0800

    Update Cornavirus_rates.py

--- Practical6/Cornavirus_rates.py
+++ Practical6/Cornavirus_rates.py
@@ -1,16 +1,18 @@
-# 1. make a dic including cases
-# 2. define lables, explode, sizes
-# 3. draw a pie diagram
+# 1. import matplotlib
+# 2. make a dic including cases
+# 3. calculate important values
+# 4. define lables, explode, sizes
+# 5. draw a pie diagram
 
 import matplotlib.pyplot as plt
 dic={'USA':29862124,'India':11285561,'Brazil':11205972,'Russia':4360823,'UK':4234924}
-sum= dic['USA']+dic['India']+dic['Brazil']+dic['Russia']+dic['UK']
+sum= dic['USA']+dic['India']+dic['Brazil']+dic['Russia']+dic['UK']        # the sum of all cases
 labels='USA','India','Brazil','Russia','UK'
-sizes=[100*dic['USA']/sum,100*dic['India']/sum,100*dic['Brazil']/sum,100*dic['Russia']/sum,100*dic['UK']/sum]
+sizes=[100*dic['USA']/sum,100*dic['India']/sum,100*dic['Brazil']/sum,100*dic['Russia']/sum,100*dic['UK']/sum]    # the percentage of cases is count by "100*X/sum"
 explode=(0,0.1,0,0,0)
 plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=False,startangle=90)
 plt.axis('equal')
-plt.show()
+plt.show()        # make the plot and draw
 
 
 
