e144129262bccf2c3b6b2b6b328f8e3f51dcbe89
DongYut <79961144+DongYut@users.noreply.github.com>
2021-04-09 19:27:10 +0800

    Update get_unknow_function.py

--- Practical8/get_unknow_function.py
+++ Practical8/get_unknow_function.py
@@ -1,11 +1,15 @@
-
+# 1. Read file and import os and re
+# 2. Firstly find the name part (starts with >)
+# 3. Make the sequnce in one line
+# 4. Find 'unknown function' DNA and write into new file
+# 5. Rewrite the title of DNA 
 
 import os 
 import re
 os.chdir('Practical8')
 with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r') as f:
   read = f.read()
-  read = read.replace('\n','').replace('>','\n>')
+  read = read.replace('\n','').replace('>','\n>')        # make sequce in one line
 gene=open('unknown_function.fa','w')
 gene.write(read)
 gene.close
@@ -16,7 +20,7 @@ for line in g:
     if 'unknown' in line:
         output=output + line
 g.close
-rewrite=open('unknown_function.fa','w')
+rewrite=open('unknown_function.fa','w')     # create new file and write for first time
 rewrite.write(output)
 rewrite.close
 rewrite=open('unknown_function.fa','r')
@@ -24,10 +28,10 @@ output2=''
 length=[]
 count=''
 for line in rewrite:
-      count=re.sub(r'.+]','',line)
+      count=re.sub(r'.+]','',line)              
       sep=re.sub(r']',r'\n',line)
       length.append(len(count))
-      output2= output2+ sep
+      output2= output2+ sep               # find the length of target DNA and save them in a list
 rewrite.close
 rewrite2=open('unknown_function.fa','w')
 rewrite2.write(output2)
@@ -38,7 +42,7 @@ name2=''
 output3=''
 for line in rewrite3:
     if 'unknown' in line:
-        name=re.search(r'gene:(\S+)',line)
+        name=re.search(r'gene:(\S+)',line)                       # combine the new title in front of target DNA
         name2=name.group(1)+'      '+str(length[z]-1)
         z=z+1
         title=re.sub(r'.+',name2,line)
@@ -46,7 +50,7 @@ for line in rewrite3:
         if 'T' in line:
           
           output3=output3+title+line
-final=open('unknown_function.fa','w')
+final=open('unknown_function.fa','w')                        # write the file for second time 
 final.write(output3)
 
 
